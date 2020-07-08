a5b66e600ab9af8e0cc10e8da9ab410dc216688e
Billy Leung <billy.kfleung@gmail.com>
2020-07-08 23:01:04 +0800

    added Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "Itamomo", category: "italian", address: "Rome")
+Restaurant.create(name: "China Town", category: "chinese", address: "HK")
+Restaurant.create(name: "Angry Sushi", category: "japanese", address: "Kyoto")
+Restaurant.create(name: "Pret a manger", category: "french", address: "paris")
+Restaurant.create(name: "Manneken Pis", category: "belgian", address: "Brussels")
